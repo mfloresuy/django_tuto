c3fd5bbf36fa4a7ee6759a11d8beb4f2f5fd2927
Martin Flores <cachopunk@gmail.com>
2016-09-11 20:38:38 -0300

    Hasta tutorial04 inclusive

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,23 +7,24 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Question(models.Model):
-	"""docstring for Question"""
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
+    """docstring for Question"""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
-	def __unicode__(self):
-		return self.question_text
+    def __unicode__(self):
+        return self.question_text
 
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
-	"""docstring for Choice"""
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+    """docstring for Choice"""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-	def __unicode__(self):
-		return self.choice_text
+    def __unicode__(self):
+        return self.choice_text
